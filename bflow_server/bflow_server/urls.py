cf2c7e679d3190a290d51370b28129d0f3cd9b50
Roseline Anne <tekehanneroseline@gmail.com>
2026-07-06 14:51:21 +0100

    chore: add EAS build configuration

--- bflow_server/bflow_server/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import include, path
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/accounts/', include('accounts.urls')),
-]
